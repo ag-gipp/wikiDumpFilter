bdb9d6e7487aaab499a15a730990702617078d9e
Dennis Halbach <dennis.halbach@uni-wuppertal.de>
2019-12-01 00:34:02 +0100

    List of significant languages

---
+++ wikidumpfilter/constants.py
@@ -0,0 +1,15 @@
+# copied from https://meta.wikimedia.org/wiki/List_of_Wikipedias (only exchanged "-" with "_" and used "be_x_old"
+# instead of "be_tarask");   if you need to update this: copy the table from the link and paste it to Excel to
+# extract the language-codes easily
+language_codes = "en,ceb,sv,de,fr,nl,ru,it,es,pl,war,vi,ja,zh,pt,ar,uk,fa,sr,ca,no,id,ko,fi,hu,sh,cs,ro,eu,tr,ms,eo," \
+                 "hy,bg,da,he,ce,sk,zh_min_nan,kk,min,hr,et,lt,be,el,sl,gl,azb,nn,az,simple,ur,th,hi,ka,uz,la,ta,vo," \
+                 "cy,mk,ast,tg,lv,mg,tt,oc,af,bs,ky,sq,tl,zh_yue,bn,new,te,be_x_old,br,ml,pms,su,lb,ht,jv,nds,sco,mr," \
+                 "sw,ga,ba,is,pnb,my,fy,cv,lmo,an,ne,yo,pa,bar,io,gu,als,ku,scn,bpy,kn,ckb,ia,wuu,qu,arz,mn,bat_smg," \
+                 "wa,si,or,yi,am,gd,cdo,nap,bug,mai,hsb,ilo,map_bms,fo,xmf,mzn,li,vec,sd,eml,sah,os,sa,diq,ps,mrj," \
+                 "mhr,zh_classical,hif,nv,roa_tara,ace,bcl,frr,hak,szl,pam,nso,km,se,hyw,rue,mi,nah,vls,bh,nds_nl," \
+                 "crh,gan,vep,sc,ab,as,bo,glk,myv,co,tk,fiu_vro,so,lrc,kv,csb,shn,gv,sn,udm,zea,ay,ie,pcd,nrm,kab,ug," \
+                 "stq,lez,kw,ha,lad,mwl,haw,gom,gn,rm,lij,lfn,lo,koi,mt,frp,fur,dsb,ext,ang,ln,dty,olo,cbk_zam,dv," \
+                 "ksh,bjn,gag,pi,pfl,pag,av,bxr,gor,xal,krc,za,pap,kaa,pdc,tyv,rw,to,kl,nov,arc,jam,kbp,kbd,tpi,tet," \
+                 "ig,ki,zu,wo,na,sat,jbo,roa_rup,lbe,bi,ty,mdf,kg,lg,tcy,srn,inh,xh,atj,ltg,chr,sm,pih,om,tn,ak,cu," \
+                 "ts,tw,rmy,bm,st,chy,rn,got,tum,ny,ss,ch,pnt,fj,iu,ady,ve,ee,ks,ik,sg,ff,dz,ti,din,cr,ng,cho,kj,mh," \
+                 "ho,ii,aa,mus,hz,kr".split(',')
